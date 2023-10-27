4c2cb0403fd93918a1bb6b1f273408cffe96df5e
kimminsubminkim <minsub2003@naver.com>
2023-10-27 16:51:56 +0900

    주석지우기

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-/*
-	Paradigm Shift by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function ($) {
 
 	var $window = $(window),
 		$body = $('body');
 
-	// Breakpoints.
+
 	breakpoints({
 		default: ['1681px', null],
 		xlarge: ['1281px', '1680px'],
@@ -20,28 +14,23 @@
 		xxsmall: [null, '360px']
 	});
 
-	// Play initial animations on page load.
 	$window.on('load', function () {
 		window.setTimeout(function () {
 			$body.removeClass('is-preload');
 		}, 100);
 	});
 
-	// Hack: Enable IE workarounds.
 	if (browser.name == 'ie')
 		$body.addClass('is-ie');
 
-	// Mobile?
 	if (browser.mobile)
 		$body.addClass('is-mobile');
 
-	// Scrolly.
 	$('.scrolly')
 		.scrolly({
 			offset: 100
 		});
 
-	// Polyfill: Object fit.
 	if (!browser.canUse('object-fit')) {
 
 		$('.image[data-position]').each(function () {
@@ -49,14 +38,12 @@
 			var $this = $(this),
 				$img = $this.children('img');
 
-			// Apply img as background.
 			$this
 				.css('background-image', 'url("' + $img.attr('src') + '")')
 				.css('background-position', $this.data('position'))
 				.css('background-size', 'cover')
 				.css('background-repeat', 'no-repeat');
 
-			// Hide img.
 			$img
 				.css('opacity', '0');
 
@@ -67,14 +54,13 @@
 			var $this = $(this),
 				$img = $this.children('img');
 
-			// Apply img as background.
+
 			$this
 				.css('background-image', 'url("' + $img.attr('src') + '")')
 				.css('background-position', 'center')
 				.css('background-size', 'cover')
 				.css('background-repeat', 'no-repeat');
 
-			// Hide img.
 			$img
 				.css('opacity', '0');
 
@@ -82,7 +68,7 @@
 
 	}
 
-	// Gallery.
+
 	$('.gallery')
 		.on('click', 'a', function (event) {
 
@@ -91,35 +77,30 @@
 				$modal = $gallery.children('.modal'),
 				$modalImg = $modal.find('img'),
 				href = $a.attr('href');
-
-			// Not an image? Bail.
 			if (!href.match(/\.(jpg|gif|png|mp4)$/))
 				return;
 
-			// Prevent default.
+
 			event.preventDefault();
 			event.stopPropagation();
 
-			// Locked? Bail.
+
 			if ($modal[0]._locked)
 				return;
 
-			// Lock.
+
 			$modal[0]._locked = true;
 
-			// Set src.
+
 			$modalImg.attr('src', href);
 
-			// Set visible.
 			$modal.addClass('visible');
 
-			// Focus.
 			$modal.focus();
 
-			// Delay.
 			setTimeout(function () {
 
-				// Unlock.
+
 				$modal[0]._locked = false;
 
 			}, 600);
@@ -130,25 +111,24 @@
 			var $modal = $(this),
 				$modalImg = $modal.find('img');
 
-			// Locked? Bail.
+
 			if ($modal[0]._locked)
 				return;
 
-			// Already hidden? Bail.
+
 			if (!$modal.hasClass('visible'))
 				return;
 
-			// Stop propagation.
 			event.stopPropagation();
 
-			// Lock.
+
 			$modal[0]._locked = true;
 
-			// Clear visible, loaded.
+
 			$modal
 				.removeClass('loaded')
 
-			// Delay.
+
 			setTimeout(function () {
 
 				$modal
@@ -156,13 +136,12 @@
 
 				setTimeout(function () {
 
-					// Clear src.
+
 					$modalImg.attr('src', '');
 
-					// Unlock.
 					$modal[0]._locked = false;
 
-					// Focus.
+
 					$body.focus();
 
 				}, 475);
@@ -174,14 +153,14 @@
 
 			var $modal = $(this);
 
-			// Escape? Hide modal.
+
 			if (event.keyCode == 27)
 				$modal.trigger('click');
 
 		})
 		.on('mouseup mousedown mousemove', '.modal', function (event) {
 
-			// Stop propagation.
+
 			event.stopPropagation();
 
 		})
@@ -194,11 +173,8 @@
 
 			setTimeout(function () {
 
-				// No longer visible? Bail.
 				if (!$modal.hasClass('visible'))
 					return;
-
-				// Set loaded.
 				$modal.addClass('loaded');
 
 			}, 275);
